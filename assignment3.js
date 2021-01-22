9362b450accce7f616c4d5a75678a65baff312e1
Avimistry4800 <avimistry4800@gmail.com>
2021-01-22 13:23:16 +0600

    hotelCost done

--- assignment3.js
+++ assignment3.js
@@ -5,11 +5,34 @@ function kilometerToMeter(kilometer){
     return reasult;
 }
 
-// #2 budgetCalculator function
-function budgetCalculator (watch,mobile,laptop){
+// #2 budgetCalculator function 
+// in this function , if you want to buy one or two product, please give 0 as input on which product you don't want to buy.
+function budgetCalculator (watch,mobile,laptop){  
     var watchCost = watch * 50 ;
     var mobileCost = mobile * 100;
     var laptopCost = laptop *500;
     var totalCost = watchCost + mobileCost + laptopCost ;
     return totalCost ;
+}
+
+// #3 hotelCost function
+function hotelCost (dayCount){
+    var totalCost = 0;
+    if (dayCount <= 10 ){
+        totalCost = dayCount * 100;
+    }
+    else if(dayCount <= 20){
+        var firstPackage = 10 * 100;
+        var remainingDays = dayCount - 10;
+        var secondPackage = remainingDays * 80;
+        var totalCost = firstPackage + secondPackage;
+    } 
+    else {
+        var firstPackage =10 * 100;
+        var secondPackage = 10 * 80;
+        var remainingDays = dayCount -20;
+        var thirdPackage = remainingDays *50 ;
+        totalCost = firstPackage + secondPackage + thirdPackage ;
+    }
+    return totalCost
 }
