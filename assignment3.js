64c99cdeef417ce91186b3dcda36662791265d3f
Avimistry4800 <avimistry4800@gmail.com>
2021-01-22 12:34:33 +0600

    kilimiterToMeter done

---
+++ assignment3.js
@@ -0,0 +1,7 @@
+//https://github.com/Avimistry4800/Assignment-3
+// #1 Kilometer to meter function
+function kilometerToMeter(kilometer){
+    var reasult = kilometer * 1000 ;
+    return reasult;
+}
+
